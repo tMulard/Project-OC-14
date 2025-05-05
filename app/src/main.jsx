1629532e4ac3e8f431e70f0c28ec610ebe1eaa73
tMulard <theo.mulard@gmail.com>
2025-05-05 12:54:50 +0200

    update

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import App from './pages/App/App.jsx'
+import Current from './pages/Current/Current.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/current" element={<Current />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ app/src/pages/Current/Current.jsx
@@ -0,0 +1,12 @@
+
+import './Current.css'
+
+function Current() {
+  
+
+  return (
+    <p>lorem ipsum</p>
+)
+}
+
+export default Current
