b6837e5490e4a3860bba981017a2d65a87f798d6
tMulard <theo.mulard@gmail.com>
2025-05-15 13:38:26 +0200

    Fixed modal pre-packaging

--- modale/src/App.jsx
+++ modale/src/App.jsx
@@ -1,14 +1,31 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Modal from "./components/Modal";
 
 function App() {
+  const [isHidden, setIsHidden] = useState(true);
+
+  const handleClick = () => {
+    setIsHidden(!isHidden);
+  };
 
   return (
     <>
-      
-        
+      <button className="modaleBtn" onClick={handleClick}>
+        Modal
+      </button>
+
+      <Modal isHidden={isHidden} isHiddenCross={false} toggleDisplay={handleClick}>
+        <h1>Bonjour</h1>
+      </Modal>
+
+      {/* <Modal
+        bordorColor="red"      // optionnel
+        transition={500}       // optionnel mais avec une valeur par default de 300ms
+        displayCross={false}   // par default true
+      />  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
